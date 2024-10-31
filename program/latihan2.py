f0602ca269188f7c23f8277fef4f6f822ebb73fc
laLafid <dzaky878alhafidz@gmail.com>
2024-10-31 09:30:57 +0700

    new file:   gambar/latihan2.png 	deleted:    program/latihan.py 	modified:   program/latihan2.py kommmit png

--- program/latihan2.py
+++ program/latihan2.py
@@ -1,11 +1,16 @@
-laba = [0, 0, 0.01, 0.01, 0.05, 0.05, 0.05, -0.02]
-saldo = 100_000_000
-total = 0
+modal_awal = 100_000_000
 
-for bulan, laba_bulan in enumerate(laba, start=1):
-    laba_bulan = saldo * laba_bulan
-    total += laba_bulan
-    print(f"Keuntungan bulan ke {bulan} adalah: {laba_bulan}")
+def hitung_keuntungan(modal_awal):
+  laba = [0, 0, 0.01, 0, 0.05, 0, 0, -0.02]
+  total = 0
+  for i, persentase in enumerate(laba, start=1):
+    keuntungan = modal_awal * persentase
+    total += keuntungan
+    modal_awal += keuntungan
+    print(f"Keuntungan bulan ke-{i} adalah: {modal_awal:.2f}")
+  return total
 
-print(f"total laba {total}")
-print(f"total saldo menjadi {total + saldo}")
+total = hitung_keuntungan(modal_awal)
+print(f"Modal awal: {modal_awal:.2f}")
+print(f"Total keuntungan selama 8 bulan adalah: {total:.2f}")
+print(f"Total saldo setelah 8 bulan adalah: {modal_awal + total:.2f}")
